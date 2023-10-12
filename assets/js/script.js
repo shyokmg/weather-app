8227a41d530259a4c6a6e482354229e32f0842fa
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-10-12 23:45:17 +1100

    Extract data from search result and display on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,127 @@
 const apiKey = '382e4071686a9c9505887e47a96575f7';
 
-
-
 function currentForecast(lat, lon, appid) {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`;
-    fetch(url).then(function(response){
-        if (response.status === 200){
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`;
+    fetch(url).then(function (response) {
+        if (response.status === 200) {
             return response.json();
         } else {
             document.location.replace('index.html');
         }
     })
-    .then(function(data){
-        console.log('CURRENT DAY FORECAST');
-        console.log(data);
-        getCurrentWeather(data);
-    })
+        .then(function (data) {
+            console.log('CURRENT DAY FORECAST');
+            console.log(data);
+            getCurrentWeather(data);
+        })
 }
 
 function fiveDayForecast(lat, lon, appid) {
-    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}`;
-    fetch(url).then(function(response){
-        if (response.status === 200){
+    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`;
+    fetch(url).then(function (response) {
+        if (response.status === 200) {
             return response.json();
         } else {
             document.location.replace('index.html');
         }
     })
-    .then(function(data){
-        console.log('FIVE DAY FORECAST');
-        console.log(data);
-    })
+        .then(function (data) {
+            console.log('FIVE DAY FORECAST');
+            // console.log(data);
+            let fiveResults = getFiveDayWeather(data);
+            generateFiveDayForecast(fiveResults);
+        })
 }
 
 function geoCoding(cityname, limit, appid) {
-    // let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},${countrycode}&limit=${limit}&appid=${appid}`;
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=${limit}&appid=${appid}`;
 
-    fetch(url).then(function(response){
-        if (response.status === 200){
+    fetch(url).then(function (response) {
+        if (response.status === 200) {
             return response.json();
         } else {
             document.location.replace('index.html');
         }
     })
-    .then(function(data){
-        // console.log(data);
-        let lat = data[0].lat
-        let lon = data[0].lon
-        currentForecast(lat, lon, apiKey)
-        fiveDayForecast(lat, lon, apiKey)
+        .then(function (data) {
+            // console.log(data);
+            let lat = data[0].lat
+            let lon = data[0].lon
+            currentForecast(lat, lon, apiKey)
+            fiveDayForecast(lat, lon, apiKey)
 
-    })
+        })
 }
 
-function getCurrentWeather(data){
+function getCurrentWeather(data) {
     let cityName = data.name;
     let dt = dayjs.unix(data.dt).format('MM/DD/YYYY');
-    let weatherDesc = data.weather[0].description;
+    let icon = data.weather[0].icon;
     let temp = data.main.temp;
     let wind = data.wind.speed;
     let humid = data.main.humidity;
 
-    let printData = `${cityName} (${dt}) ${weatherDesc}\n 
-    Temp: ${temp} \n
-    Wind: ${wind} \n
-    Humidity: ${humid}`
-
-    console.log(printData);
-
+    $('#cityname').text(`${cityName} (${dt})`);
+    $('#mainIcon').val();
+    $('#mainTemp').text(`Temp ${temp} °C`);
+    $('#mainWind').text(`Wind: ${wind} km/h`);
+    $('#mainHumid').text(`Humidity: ${humid}%`);
 
 
+
+}
+
+function getFiveDayWeather(data) {
+    let fiveObj = { dt: [], temp: [], humid: [], wind: [], iconID: [] }
+    let filterObj = { dt: [], temp: [], humid: [], wind: [], iconID: [] }
+
+    for (let i = 0; i < data.list.length; i++) {
+        fiveObj.dt.push(dayjs.unix(data.list[i].dt).format('MM/DD/YYYY'));
+        fiveObj.temp.push(data.list[i].main.temp);
+        fiveObj.humid.push(data.list[i].main.humidity);
+        fiveObj.wind.push(data.list[i].wind.speed);
+        fiveObj.iconID.push(data.list[i].weather[0].icon);
+    }
+
+    let filter = filterFiveResults(fiveObj.dt);
+    for (let i = 0; i < filter.length; i++) {
+        filterObj.dt.push(fiveObj.dt[filter[i]]);
+        filterObj.temp.push(fiveObj.temp[filter[i]]);
+        filterObj.humid.push(fiveObj.humid[filter[i]]);
+        filterObj.wind.push(fiveObj.wind[filter[i]]);
+        filterObj.iconID.push(fiveObj.iconID[filter[i]]);
+    }
+    console.log(fiveObj);
+    console.log(filterObj);
+    return filterObj;
+}
+
+
+function generateFiveDayForecast(data) {
+    var list = $('#weatherList');
+    for (let i = 0; i < data.dt.length; i++) {
+        let fiveList = $('<li>').addClass('box');
+        let date = $('<li>').text(data.dt[i]);
+        let icon = $(`<li><img src="https://openweathermap.org/img/wn/${data.iconID[i]}@2x.png><img></li>`);
+        let temp = $('<li>').text(`Temp: ${data.temp[i]}°C`);
+        let wind = $('<li>').text(`Wind: ${data.wind[i]} km/h`);
+        let humid = $('<li>').text(`Humidity: ${data.humid[i]}%`);
+        fiveList.append(date, icon, temp, wind, humid);       
+        list.append(fiveList);
+    }
+    
+
+}
+function filterFiveResults(arr) {
+    var someArr = [];
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[count] !== arr[i]) {
+            someArr.push(i);
+            count = i;
+        }
+    }
+    return someArr;
 }
 
 
@@ -80,17 +131,9 @@ function getCurrentWeather(data){
 
 
 
-
-
-
-
-
-
-
-
-$('#search').on('click', function(){
+$('#search').on('click', function () {
     let searchInput = $('#autocomplete').val();
-    if (searchInput !== ""){
+    if (searchInput !== "") {
         console.log(searchInput);
         geoCoding(searchInput, 1, apiKey);
     }
@@ -99,7 +142,7 @@ $('#search').on('click', function(){
 
 
 $('#autocomplete').autocomplete({
-    source: [ "Sydney", "Melbourne", "Brisbane", "Manila", "Tokyo", "Atlanta", "New York" ]
-  });
+    source: ["Sydney", "Melbourne", "Brisbane", "Manila", "Tokyo", "Atlanta", "New York"]
+});
 // geoCoding('Sydney,', 1, apiKey);
 // openWeather();
