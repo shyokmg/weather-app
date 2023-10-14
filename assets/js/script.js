dd806e91e77cab2771abaf2656052a49edc1d0ab
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-10-14 23:20:55 +1100

    Added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const apiKey = '382e4071686a9c9505887e47a96575f7';
 var savedCities = ['Sydney'];
 
+// API for current weather forecast
 function currentForecast(lat, lon, appid) {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`;
     fetch(url).then(function (response) {
@@ -15,6 +16,7 @@ function currentForecast(lat, lon, appid) {
         })
 }
 
+// API for 5day weather forecast
 function fiveDayForecast(lat, lon, appid) {
     let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`;
     fetch(url).then(function (response) {
@@ -30,6 +32,7 @@ function fiveDayForecast(lat, lon, appid) {
         })
 }
 
+// function for getting geo location using API
 function geoCoding(cityname, limit, appid) {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=${limit}&appid=${appid}`;
 
@@ -44,6 +47,7 @@ function geoCoding(cityname, limit, appid) {
             let lat = data[0].lat
             let lon = data[0].lon
             let saveCity = data[0].name
+            // Check saved city history and save to local storage
             if(!savedCities.includes(saveCity)){
                 savedCities.push(saveCity);
                 localStorage.setItem('savedCities', JSON.stringify(savedCities));
@@ -55,6 +59,7 @@ function geoCoding(cityname, limit, appid) {
         })
 }
 
+// Extract current weather forecast from given data
 function getCurrentWeather(data) {
     let cityName = data.name;
     let dt = dayjs.unix(data.dt).format('MM/DD/YYYY');
@@ -63,20 +68,21 @@ function getCurrentWeather(data) {
     let wind = data.wind.speed;
     let humid = data.main.humidity;
 
+    // Dynamically add data to html
     $('#cityname').text(`${cityName} (${dt})`);
     $('#mainIcon').addClass('icons').attr('src', icon);
     $('#mainTemp').text(`Temp ${temp} °C`);
     $('#mainWind').text(`Wind: ${wind} km/h`);
     $('#mainHumid').text(`Humidity: ${humid}%`);
-    
-
-
 }
 
+// Extract 5day weather forecast from given data
 function getFiveDayWeather(data) {
+    // Assign data to object
     let fiveObj = { dt: [], temp: [], humid: [], wind: [], iconID: [] }
     let filterObj = { dt: [], temp: [], humid: [], wind: [], iconID: [] }
 
+    // loops through data and adds to fiveObj
     for (let i = 0; i < data.list.length; i++) {
         fiveObj.dt.push(dayjs.unix(data.list[i].dt).format('MM/DD/YYYY'));
         fiveObj.temp.push(data.list[i].main.temp);
@@ -85,6 +91,7 @@ function getFiveDayWeather(data) {
         fiveObj.iconID.push(data.list[i].weather[0].icon);
     }
 
+    // lopps through the fiveObj and filters data to managable results
     let filter = filterFiveResults(fiveObj.dt);
     for (let i = 0; i < filter.length; i++) {
         filterObj.dt.push(fiveObj.dt[filter[i]]);
@@ -96,7 +103,7 @@ function getFiveDayWeather(data) {
     return filterObj;
 }
 
-
+// function that dynamically adds weather cards for 5day forecast
 function generateFiveDayForecast(data) {
     var list = $('#weatherList');
     for (let i = 0; i < data.dt.length; i++) {
@@ -114,6 +121,8 @@ function generateFiveDayForecast(data) {
     
 
 }
+
+// function to reduce results to managable data
 function filterFiveResults(arr) {
     let someArr = [];
     let count = 0;
@@ -126,12 +135,13 @@ function filterFiveResults(arr) {
     return someArr;
 }
 
-
+// Click event for search
 $('#search').on('click', function (event) {
     event.preventDefault();
-    let searchInput = $('#autocomplete').val();
     $('#weatherList').empty();
     $('#historyList').empty();
+
+    let searchInput = $('#autocomplete').val();
     if (searchInput !== "") {
         geoCoding(searchInput, 1, apiKey);
     }
@@ -139,19 +149,20 @@ $('#search').on('click', function (event) {
 });
 
 
-
+// Added auto complete function that gets data from saved history
 $('#autocomplete').autocomplete({
     source: savedCities
 });
 
 
-
+// Create a button list of saved cities from history
 function createHistory(arr){
     let historyList = $('#historyList');
     for (let i = 0; i < arr.length; i++){
         let cityButton = $(`<button class="btn btn-secondary btn-sm cityButton" data-city=${savedCities[i]}>${savedCities[i]}</button>`);
         historyList.append(cityButton);
     }
+    // Click event on city buttons 
     $('.cityButton').on('click', function(event){
         let element = event.target;
         if(element.matches('button')){
@@ -163,6 +174,7 @@ function createHistory(arr){
     });
 }
 
+// initialize page with default city (Syndey) and get  history from local storage;
 function init(){
     geoCoding("Sydney", 1, apiKey);
     let check = JSON.parse(localStorage.getItem('savedCities'));
@@ -171,7 +183,5 @@ function init(){
     } else {
         localStorage.setItem('savedCities', JSON.stringify(savedCities));
     }
-
-    
 }
 init();
